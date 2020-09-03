21b61922ec084cc6dd78aaa0af9d49498b77dede
Tsimafei Shchytkavets <timoxino@gmail.com>
2020-09-03 15:59:29 -0400

    add empty spring boot app

---
+++ src/main/java/com/timoxino/tryout/springboot/admin/SpringBootAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.timoxino.tryout.springboot.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootAdminServerApplication.class, args);
+  }
+
+}
